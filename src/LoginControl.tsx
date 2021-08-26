883692e290b13249adca74196a77ca56ed1cd1a5
Dave Gentilli <dgentilli@gmail.com>
2021-08-26 12:43:30 +0300

    Implement basic logic for displaying request password reset component.

--- src/LoginControl.tsx
+++ src/LoginControl.tsx
@@ -1,15 +1,32 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import Signup from './Signup';
+import PasswordResetRequest from './PasswordResetReqeust';
 
 const LoginControl = () => {
   const [isUser, setIsUser] = useState(false);
+  const [displayReset, setDisplayReset] = useState(false);
 
-  if (isUser) {
-    return <Login />;
-  } else {
-    return <Signup />;
-  }
+  const buttonText = isUser
+    ? "Don't have an account?"
+    : 'Already have an account?';
+
+  const toggleIsUser = (): void => {
+    setIsUser(!isUser);
+  };
+
+  const toggleDisplayReset = (): void => {
+    setDisplayReset(!displayReset);
+  };
+
+  return (
+    <div>
+      {isUser ? <Login /> : <Signup />}
+      <button onClick={toggleIsUser}>{buttonText}</button>
+      <p onClick={toggleDisplayReset}>Forgot Password?</p>
+      {displayReset ? <PasswordResetRequest /> : null}
+    </div>
+  );
 };
 
 export default LoginControl;
---
+++ src/PasswordResetReqeust.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PasswordResetRequest = () => {
+  return <div>Reset Password?</div>;
+};
+
+export default PasswordResetRequest;

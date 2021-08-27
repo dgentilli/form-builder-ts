e5303235c43303539ecd297d4489cd89285a3bf7
Dave Gentilli <dgentilli@gmail.com>
2021-08-27 17:26:35 +0300

    Test concept with the Login validations.

---
+++ src/Login/Login.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormWrapper from '../form/FormWrapper';
+import { Field } from '../types/globalTypes';
+import PasswordResetRequest from './PasswordResetReqeust';
+
+const Login = () => {
+  return (
+    <div>
+      <h2>Login</h2>
+      <FormWrapper
+        fields={[Field.Email, Field.Password]}
+        validations={[Field.Email, Field.Password]}
+      />
+    </div>
+  );
+};
+
+export default Login;

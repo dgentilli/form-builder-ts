dc487ce5120e2fd107af38108462f6c6fdbd606a
Dave Gentilli <dgentilli@gmail.com>
2021-08-27 17:24:01 +0300

    Establish some types to be used throughout the app.

---
+++ src/types/globalTypes.ts
@@ -0,0 +1,12 @@
+export enum Field {
+  Email = 'email',
+  Password = 'password',
+  ConfirmPassword = 'confirm password',
+  FirstName = 'first name',
+  LastName = 'last name',
+}
+
+export interface BasicFormProps {
+  fields: Field[];
+  validations?: Field[];
+}

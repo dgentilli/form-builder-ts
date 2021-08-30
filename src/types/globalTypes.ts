6fc72d42486c227cda35cd01293aead96020fa3d
Dave Gentilli <dgentilli@gmail.com>
2021-08-30 13:15:10 +0300

    Remove unused code.

--- src/types/globalTypes.ts
+++ src/types/globalTypes.ts
@@ -1,13 +1,11 @@
 export enum Field {
   Email = 'email',
   Password = 'password',
-  ConfirmPassword = 'confirm password',
   FirstName = 'first name',
   LastName = 'last name',
 }
 
 export interface BasicFormProps {
   fields: Field[];
-  validations?: Field[];
   requiredFields?: Field[];
 }
